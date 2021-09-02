c1f271f36531cf75ac89bda1391431e3ac495044
Stefan van Tilborg <stefan.vantilborg@floryn.com>
2021-09-02 15:08:59 +0200

    rewrite code in more efficient way

--- app/controllers/ex3_controller.rb
+++ app/controllers/ex3_controller.rb
@@ -4,51 +4,60 @@ class Ex3Controller < ApplicationController
     file = File.open('inputs/inputEx3.txt')
     input = file.read
 
-    def parse_input(input, right, down)
-      visual_map = ''
-      trees = 0
-      x = 0
-      y = 0
-
-      input.each_line do |line|
-        if y % down == 0 # Succifient because down will be 1 or 2
-          line = line.delete("\n") # Remove newline
-          multiplier = (x + 1 / line.length).ceil # Calculate how much we are missing
-          line *= multiplier # Multiply line until it's long enough
-
-          if line[x] == '#' # Check if there is a tree
-            trees += 1
-          end
-
-          line[x] = 'X' # Replace tree with X for the visual
-          visual_map += "\n#{line}" # Add newline to visual
-          x += right # Move X position
-        end
-        y += 1 # Increment y after function because we always process line 1
-      end
-      [trees, visual_map]
-    end
-
     #### Part 1 ####
-    part1 = parse_input(input, 3, 1)
-    @trees = part1[0]
-    @out = part1[1]
+    @trees1 = parse_input(input, 3, 1)
+    @out = visualiser(input, 3, 1)
 
     #### Part 2 ####
-    def do_part2 (input)
-      out1 = parse_input(input, 1, 1)[0]
-      out2 = parse_input(input, 3, 1)[0]
-      out3 = parse_input(input, 5, 1)[0]
-      out4 = parse_input(input, 7, 1)[0]
-      out5 = parse_input(input, 1, 2)[0]
-      out1 * out2 * out3 * out4 * out5
-    end
-
     file_ex = File.open('inputs/inputEx3.1.txt')
-    input_ex = file_ex.read
-    @example = do_part2(input_ex)
+    @trees_example = do_part2(file_ex.read)
+    @trees2 = do_part2(input)
 
-    @part2 = do_part2(input)
+  end
 
+  private
+
+  def parse_input(input, right, down = 1)
+    trees = 0
+    x = 0
+    y = 0
+
+    input.each_line do |line|
+      if y % down == 0 # Succifient because down will be 1 or 2
+        line = line.delete("\n") # Remove newline
+        x = x >= line.length ? x % line.length : x # Trim x
+        trees += line[x] == '#' ? 1 : 0 # Check if there is a tree, incr. trees
+        line[x] = 'X' # Replace step with X for the visual
+        x += right # Move X position
+      end
+      y += 1 # Increment y after function because we always process line 1
+    end
+    trees
+  end
+
+  def do_part2 (input)
+    parse_input(input, 1) *
+    parse_input(input, 3) *
+    parse_input(input, 5) *
+    parse_input(input, 7) *
+    parse_input(input, 1, 2)
+  end
+
+  def visualiser(input, right, down)
+    visual_map = ''
+    x = 0
+    y = 0
+
+    input.each_line do |line|
+      if y % down == 0 # Succifient because down will be 1 or 2
+        line = line.delete("\n") # Remove newline
+        x = x >= line.length ? x % line.length : x # Trim x
+        line[x] = 'X' # Replace tree with X for the visual
+        visual_map += "\n#{line}" # Add newline to visual
+        x += right # Move X position
+      end
+      y += 1 # Increment y after function because we always process line 1
+    end
+    visual_map
   end
 end
