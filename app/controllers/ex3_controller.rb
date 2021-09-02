4a6371602b7b318598f2373ad2307be842fd60f8
Stefan van Tilborg <stefan.vantilborg@floryn.com>
2021-09-02 13:43:38 +0200

    Add rubocop, apply conventions to ex3

--- app/controllers/ex3_controller.rb
+++ app/controllers/ex3_controller.rb
@@ -1,56 +1,54 @@
 class Ex3Controller < ApplicationController
   def index
 
-    file = File.open("../inputEx3.txt")
+    file = File.open('../inputEx3.txt')
     input = file.read
 
-    def parseInput(input, right, down)
-      visualMap = ""
+    def parse_input(input, right, down)
+      visual_map = ''
       trees = 0
       x = 0
       y = 0
 
       input.each_line do |line|
-        if(y %down !=  0) # Succifient because down will be 1 or 2
-          y = y + 1
-        else
-          line = line.gsub("\n",'') # Remove newline
+        if y % down == 0 # Succifient because down will be 1 or 2
+          line = line.delete("\n") # Remove newline
           multiplier = (x + 1 / line.length).ceil # Calculate how much we are missing
-          line = line * multiplier # Multiply line until it's long enough
-          
-          if(line[x] == "#") # Check if there is a tree
-            trees = trees + 1
+          line *= multiplier # Multiply line until it's long enough
+
+          if line[x] == '#' # Check if there is a tree
+            trees += 1
           end
 
-          line[x] = "X" # Replace tree with X for the visual
-          visualMap = visualMap + "\n" + line  # Add newline to visual
-          x = x + right # Move X position
-          y = y + 1 # Increment y after function because we always process line 1
+          line[x] = 'X' # Replace tree with X for the visual
+          visual_map += "\n#{line}" # Add newline to visual
+          x += right # Move X position
         end
+        y += 1 # Increment y after function because we always process line 1
       end
-      return trees, visualMap
+      [trees, visual_map]
     end
 
     #### Part 1 ####
-    part1 = parseInput(input, 3, 1)
+    part1 = parse_input(input, 3, 1)
     @trees = part1[0]
     @out = part1[1]
 
     #### Part 2 ####
-    def doPart2(input)
-      out1 = parseInput(input, 1, 1)[0]
-      out2 = parseInput(input, 3, 1)[0]
-      out3 = parseInput(input, 5, 1)[0]
-      out4 = parseInput(input, 7, 1)[0]
-      out5 = parseInput(input, 1, 2)[0]
-      return out1 * out2 * out3 * out4 * out5
+    def do_part2 (input)
+      out1 = parse_input(input, 1, 1)[0]
+      out2 = parse_input(input, 3, 1)[0]
+      out3 = parse_input(input, 5, 1)[0]
+      out4 = parse_input(input, 7, 1)[0]
+      out5 = parse_input(input, 1, 2)[0]
+      out1 * out2 * out3 * out4 * out5
     end
 
-    fileEx = File.open("../inputEx3.1.txt")
-    inputEx = fileEx.read
-    @example = doPart2(inputEx)
+    file_ex = File.open('../inputEx3.1.txt')
+    input_ex = file_ex.read
+    @example = do_part2(input_ex)
 
-    @part2 = doPart2(input)
+    @part2 = do_part2(input)
 
   end
 end
